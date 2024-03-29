070cd66a31907b37b7788c0664413b49e4278bc1
gonzobard777 <gonzo.bard@dwfe.ru>
2024-03-29 08:37:39 +0300

    .

--- src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
+++ src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
@@ -16,7 +16,7 @@ export class PatternOnCanvasController {
   render() {
     drawAsBezierInterpolation(this.context, points);
     const svgProps = pathProps.svgPathProperties(getPathAsBezierInterpolation(points));
-    drawSegmentsPerpendicularToPoint(this.context, svgProps, triangle, {strokeStyle: 'red', fillStyle: 'red'});
+    drawPattern(triangle, 20, 35, {strokeStyle: 'red', fillStyle: 'red'}, this.context, svgProps);
   }
 
   dispose() {
@@ -32,24 +32,33 @@ const triangle: IPoint[] = [
   [-10, 0],
 ];
 
-function drawSegmentsPerpendicularToPoint(
+function drawPattern(
+  pattern: IPoint[],
+  start: number, // первая точка, от которой будет отрисовам паттерн, пиксели
+  step: number, // шаг между точками отрисовки паттерна, пиксели
+  opt: ILineDrawOnCanvasOpt, // настройки для отрисовки паттерна
   context: CanvasRenderingContext2D,
   svgProps: ReturnType<typeof pathProps.svgPathProperties>,
-  points: IPoint[],
-  opt: ILineDrawOnCanvasOpt,
 ): void {
   const lineDraw = new LineDraw(context, opt);
-  for (let i = 20; i < svgProps.getTotalLength(); i += 50) {
+  for (let i = start; i < svgProps.getTotalLength() - start; i += step) {
     const point = svgProps.getPointAtLength(i);
     const tangent = svgProps.getTangentAtLength(i);
-    const rotateAtPointAngle = Math.atan2(tangent.y, tangent.x);
+    const tangentAngle = Math.atan2(tangent.y, tangent.x); // угол наклона касательной
     const conv = Matrix.multiply(
-      Operator.rotateAtPoint([point.x, point.y], rotateAtPointAngle, 'rad'), // (2) повернуть
-      [1, 0, 0, 1, point.x, point.y],                                        // (1) переместить все точки
+      Operator.rotateAtPoint([point.x, point.y], tangentAngle, 'rad'), // (2) повернуть
+      [1, 0, 0, 1, point.x, point.y],                                  // (1) переместить все точки
     );
     lineDraw.run(
-      points.map(p => Matrix.apply(conv, p))
+      pattern.map(p => Matrix.apply(conv, p))
     );
+
+    // касательная прямая
+    // const k = Math.tan(tangentAngle);
+    // const b = point.y - k * point.x;
+    // const x1 = point.x + 200;
+    // const x2 = point.x - 200;
+    // drawLine(context, [[x1, k * x1 + b], [x2, k * x2 + b]], {strokeStyle: 'blue'});
   }
 }
 
