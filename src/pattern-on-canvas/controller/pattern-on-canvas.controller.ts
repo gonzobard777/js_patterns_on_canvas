460988eace530c2d85aa0dc80cfdd19d4c504d1e
gonzobard777 <gonzo.bard@dwfe.ru>
2024-03-30 09:28:15 +0300

    .

--- src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
+++ src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
@@ -1,10 +1,9 @@
-import {Matrix, Operator} from "@do-while-for-each/math";
 import * as pathProps from 'svg-path-properties';
 import {drawAsBezierInterpolation, getPathAsBezierInterpolation} from "../../app-common/bezier-interpolation";
+import {drawPatternAlongStroke} from "../../app-common/pattern/draw-pattern-along-stroke";
 import {PointBasedPattern} from "../../app-common/pattern/point-based.pattern";
 import {LineBasedPattern} from "../../app-common/pattern/line-based.pattern";
 import {ArcBasedPattern} from "../../app-common/pattern/arc-based.pattern";
-import {IPattern} from "../../app-common/pattern/contract";
 import {points} from "../../app-common/constant";
 
 export class PatternOnCanvasController {
@@ -46,39 +45,3 @@ export class PatternOnCanvasController {
 }
 
 
-/**
- * Рисовать паттерн вдоль обводки.
- * @param pattern        - паттерн, который надо нарисовать
- * @param offsetFromEdge - отступ от краев, пиксели (по линии обводки)
- * @param step           - шаг между паттернами, пиксели (по линии обводки)
- * @param strokeExtends  - в этот объект парсится обводка. В нем реализованы полезные методы.
- */
-function drawPatternAlongStroke(
-  pattern: IPattern,
-  offsetFromEdge: number,
-  step: number,
-  strokeExtends: ReturnType<typeof pathProps.svgPathProperties>,
-): void {
-  for (let len = offsetFromEdge; len < strokeExtends.getTotalLength() - offsetFromEdge; len += step) {
-    const {x, y} = strokeExtends.getPointAtLength(len);
-
-    // угол наклона касательной
-    const tangent = strokeExtends.getTangentAtLength(len);
-    const tangentAngle = Math.atan2(tangent.y, tangent.x);
-
-    // конвертер для позиционирования точек паттерна
-    const conv = Matrix.multiply(
-      Operator.rotateAtPoint([x, y], tangentAngle, 'rad'), // (2) повернуть
-      [1, 0, 0, 1, x, y],                                  // (1) переместить все точки
-    );
-
-    pattern.draw(conv);
-
-    // прямая касательная к точке на обводке
-    // const k = Math.tan(tangentAngle);
-    // const b = point.y - k * point.x;
-    // const x1 = point.x + 200;
-    // const x2 = point.x - 200;
-    // drawLine(context, [[x1, k * x1 + b], [x2, k * x2 + b]], {strokeStyle: 'blue'});
-  }
-}
