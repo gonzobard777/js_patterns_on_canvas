1cc340da8f79c1208b08f1dce07d281ae3920981
gonzobard777 <gonzo.bard@dwfe.ru>
2024-03-30 04:48:01 +0300

    .

--- src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
+++ src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
@@ -1,6 +1,7 @@
 import {Matrix, Operator} from "@do-while-for-each/math";
 import * as pathProps from 'svg-path-properties';
 import {drawAsBezierInterpolation, getPathAsBezierInterpolation} from "../../app-common/bezier-interpolation";
+import {PointBasedPattern} from "../../app-common/pattern/point-based.pattern";
 import {LineBasedPattern} from "../../app-common/pattern/line-based.pattern";
 import {ArcBasedPattern} from "../../app-common/pattern/arc-based.pattern";
 import {IPattern} from "../../app-common/pattern/contract";
@@ -26,10 +27,16 @@ export class PatternOnCanvasController {
     drawPatternAlongStroke(lineBasedPattern, 20, 35, svgProps);
 
     const arcBasedPattern = new ArcBasedPattern(
-      {center: [0, 0], radius: 7, startAngle: Math.PI, endAngle: 2 * Math.PI, strokeStyle: 'black'},
+      {center: [0, 0], radius: 6, startAngle: Math.PI, endAngle: 2 * Math.PI, strokeStyle: 'black'},
       this.context
     );
     drawPatternAlongStroke(arcBasedPattern, 20, 35, svgProps);
+
+    const pointBasedPattern = new PointBasedPattern(
+      {points, radius: 3, lineWidth: 2, strokeStyle: 'green', fillStyle: 'white'},
+      this.context
+    );
+    pointBasedPattern.draw();
   }
 
   dispose() {
