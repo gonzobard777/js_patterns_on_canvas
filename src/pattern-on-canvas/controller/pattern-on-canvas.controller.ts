a8a3f9a25b3e8ba31313f7aad0f77fcb4a604305
gonzobard777 <gonzo.bard@dwfe.ru>
2024-04-01 05:17:40 +0300

    .

--- src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
+++ src/pattern-on-canvas/controller/pattern-on-canvas.controller.ts
@@ -1,6 +1,6 @@
 import * as pathProps from 'svg-path-properties';
 import {drawAsBezierInterpolation, getPathAsBezierInterpolation} from "../../app-common/bezier-interpolation";
-import {drawPatternAlongStroke} from "../../app-common/pattern/draw-pattern-along-stroke";
+import {drawPatternAlongStroke, getConverter} from "../../app-common/pattern/draw-pattern-along-stroke";
 import {PointBasedPattern} from "../../app-common/pattern/point-based.pattern";
 import {LineBasedPattern} from "../../app-common/pattern/line-based.pattern";
 import {ArcBasedPattern} from "../../app-common/pattern/arc-based.pattern";
@@ -37,6 +37,38 @@ export class PatternOnCanvasController {
     );
     pointBasedPattern.draw();
   }
+
+  render2() {
+    const svgProps = pathProps.svgPathProperties(getPathAsBezierInterpolation(points));
+    const totalLen = svgProps.getTotalLength();
+
+    const patternBaseLen = 20;
+    const offset = 15;
+    const segmentLen = patternBaseLen + offset * 2;
+    this.context.setLineDash([segmentLen])
+    drawAsBezierInterpolation(this.context, points, 'blue');
+    const pattern1 = new LineBasedPattern(
+      {points: [[-10, 0], [0, -10], [10, 0], [-10, 0]], strokeStyle: 'blue', fillStyle: 'blue'},
+      this.context
+    );
+    for (let len = segmentLen / 2; len < totalLen; len += segmentLen * 2) {
+      const conv = getConverter(len, svgProps);
+      pattern1.draw(conv);
+    }
+
+    this.context.lineDashOffset = segmentLen;
+    this.context.setLineDash([segmentLen])
+    drawAsBezierInterpolation(this.context, points, 'red');
+    const pattern2 = new LineBasedPattern(
+      {points: [[-10, 0], [0, 10], [10, 0], [-10, 0]], strokeStyle: 'red', fillStyle: 'red'},
+      this.context
+    );
+    for (let len = segmentLen * 1.5; len < totalLen; len += segmentLen * 2) {
+      const conv = getConverter(len, svgProps);
+      pattern2.draw(conv);
+    }
+  }
+
 
   dispose() {
 
